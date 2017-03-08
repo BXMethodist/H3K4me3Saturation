fix the mean normalization

clusterUtils.py
@@ -149,7 +149,7 @@ def quantile_normalization(array, axis=0):
         return result
 
 
-def mean_normalization(array, axis=1, target_signal=200):
+def mean_normalization(array, axis=1, target_signal=100):
     """
     :param array:  numpy array
     :param axis: normalize axis
@@ -329,7 +329,7 @@ def remove_duplicate(pairs):
     return distinct_set
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # get_split_chr("chr3", 122562770, 122562820, cutoff=25)
 
     # prefix = "/home/tmhbxx3/archive/refmap_saturation/code/"
@@ -341,11 +341,11 @@ if __name__ == "__main__":
 
     # get_split_chr('chr3', 3167850, 3169860)
     # widths = []
-    f = open("75_refmap_combined.csv", 'r')
-    info = [line.strip().split(",") for line in f.readlines() if not line.startswith(">")]
-    widths = [(int(y) - int(x))*10 for x, y in info]
-
-    print np.percentile(widths, 50)
+    # f = open("75_refmap_combined.csv", 'r')
+    # info = [line.strip().split(",") for line in f.readlines() if not line.startswith(">")]
+    # widths = [(int(y) - int(x))*10 for x, y in info]
+    #
+    # print np.percentile(widths, 50)
     # import matplotlib.pyplot as plt
     # plt.xlim((0, 10000))
     # plt.ylim((0, 200))

practice.py
@@ -4,30 +4,42 @@ import numpy as np
 # from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 # from time import time
-# import os
-
-process = 8
-chunks = []
-chromosomes = [i for i in range(23)]
-# print chromosomes
-
-chunk_size = len(chromosomes) / process
-reminder = len(chromosomes) % process
-
-# print reminder
-
-cur_index = 0
-
-for i in range(process):
-    if reminder > 0:
-        chunks.append(chromosomes[cur_index + i * chunk_size:cur_index+(i + 1) * chunk_size + 1])
-        cur_index += 1
-        reminder -= 1
-    else:
-        chunks.append(chromosomes[cur_index + i * chunk_size: cur_index + (i + 1) * chunk_size])
+import os
 
 
-print chunks
+x1 = np.arange(9.0).reshape((3, 3))
+x2 = np.arange(3.0).reshape((3, 1))
+np.multiply(x1, x2)
+print x1
+print x2
+
+print np.multiply(x1, x2)
+
+
+
+#
+# process = 8
+# chunks = []
+# chromosomes = [i for i in range(23)]
+# # print chromosomes
+#
+# chunk_size = len(chromosomes) / process
+# reminder = len(chromosomes) % process
+#
+# # print reminder
+#
+# cur_index = 0
+#
+# for i in range(process):
+#     if reminder > 0:
+#         chunks.append(chromosomes[cur_index + i * chunk_size:cur_index+(i + 1) * chunk_size + 1])
+#         cur_index += 1
+#         reminder -= 1
+#     else:
+#         chunks.append(chromosomes[cur_index + i * chunk_size: cur_index + (i + 1) * chunk_size])
+#
+#
+# print chunks
 
 # path = "/home/tmhbxx3/archive/test/"
 #